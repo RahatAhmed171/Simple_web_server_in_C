f26e9ed7edb87ff5b7ec58ebaccda88de2191f25
RahatAhmed171 <rahatahmed171@gmail.com>
2024-09-13 23:16:23 +0600

    Initial commit to web server in c

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+// #pragma comment(lib, "Ws2_32.lib") // Not needed for MinGW, link the library manually
+
+int main() {
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    // You should also clean up Winsock before exiting
+    WSACleanup();
+
+    return 0;
+}
