ecc3b0d2b2cb300607068cd2c2ac7ba75f0fbe39
RahatAhmed171 <rahatahmed171@gmail.com>
2024-09-15 22:35:04 +0600

    Client socket creation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#define DEFAULT_PORT "27015"
 
 #include <windows.h>
 #include <winsock2.h>
@@ -23,6 +24,39 @@ int main() {
 
     // You should also clean up Winsock before exiting
     WSACleanup();
+
+    struct addrinfo *result = NULL,
+                *ptr = NULL,
+                hints;
+
+ZeroMemory( &hints, sizeof(hints) );
+hints.ai_family   = AF_INET;
+hints.ai_socktype = SOCK_STREAM;
+hints.ai_protocol = IPPROTO_TCP;
+
+
+// Resolve the server address and port
+iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+if (iResult != 0) {
+    printf("getaddrinfo failed: %d\n", iResult);
+    WSACleanup();
+    return 1;
+}
+SOCKET ConnectSocket = INVALID_SOCKET;
+// Attempt to connect to the first address returned by
+// the call to getaddrinfo
+ptr=result;
+
+// Create a SOCKET for connecting to server
+ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+    ptr->ai_protocol);
+  if (ConnectSocket == INVALID_SOCKET) {
+    printf("Error at socket(): %ld\n", WSAGetLastError());
+    freeaddrinfo(result);
+    WSACleanup();
+    return 1;
+}
+
 
     return 0;
 }
