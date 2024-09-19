b91aea517ba7dbe791795dd26cb55bc7f9906e81
RahatAhmed171 <rahatahmed171@gmail.com>
2024-09-20 01:24:19 +0600

    Connecting to a server

--- main.cpp
+++ main.cpp
@@ -56,6 +56,25 @@ ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
     WSACleanup();
     return 1;
 }
+iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+if (iResult == SOCKET_ERROR) {
+    closesocket(ConnectSocket);
+    ConnectSocket = INVALID_SOCKET;
+}
+
+// Should really try the next address returned by getaddrinfo
+// if the connect call failed
+// But for this simple example we just free the resources
+// returned by getaddrinfo and print an error message
+
+freeaddrinfo(result);
+
+if (ConnectSocket == INVALID_SOCKET) {
+    printf("Unable to connect to server!\n");
+    WSACleanup();
+    return 1;
+}
+
 
 
     return 0;
