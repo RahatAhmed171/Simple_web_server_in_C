7d03133dafc1666398ee0c8a1701f3c9451fdfe1
RahatAhmed171 <rahatahmed171@gmail.com>
2024-09-20 21:47:14 +0600

    Sending and recieving data in client

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 #define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
 
 #include <windows.h>
 #include <winsock2.h>
@@ -74,6 +75,46 @@ if (ConnectSocket == INVALID_SOCKET) {
     WSACleanup();
     return 1;
 }
+
+
+int recvbuflen = DEFAULT_BUFLEN;
+
+const char *sendbuf = "this is a test";
+char recvbuf[DEFAULT_BUFLEN];
+
+int iResult;
+
+// Send an initial buffer
+iResult = send(ConnectSocket, sendbuf, (int) strlen(sendbuf), 0);
+if (iResult == SOCKET_ERROR) {
+    printf("send failed: %d\n", WSAGetLastError());
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 1;
+}
+
+printf("Bytes Sent: %ld\n", iResult);
+
+// shutdown the connection for sending since no more data will be sent
+// the client can still use the ConnectSocket for receiving data
+iResult = shutdown(ConnectSocket, SD_SEND);
+if (iResult == SOCKET_ERROR) {
+    printf("shutdown failed: %d\n", WSAGetLastError());
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 1;
+}
+
+// Receive data until the server closes the connection
+do {
+    iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+    if (iResult > 0)
+        printf("Bytes received: %d\n", iResult);
+    else if (iResult == 0)
+        printf("Connection closed\n");
+    else
+        printf("recv failed: %d\n", WSAGetLastError());
+} while (iResult > 0);
 
 
 
